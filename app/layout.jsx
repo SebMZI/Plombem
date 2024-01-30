13ddc405c13650dc6c095b704ca05aa8c49e75c6
SebMZI <49833807+SebMZI@users.noreply.github.com>
2024-01-30 14:17:22 +0100

    feat: add some seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,20 @@ export const metadata = {
   title: "Plombem - Votre Partenaire Plomberie de Confiance",
   description:
     "Découvrez les services professionnels de plomberie de Plombem. Experts en dépannage, réparation et installation de plomberie. Contactez-nous pour des solutions rapides et efficaces.",
+  openGraph: {
+    title: "Plombem - Votre Partenaire Plomberie de Confiance",
+    description:
+      "Découvrez les services professionnels de plomberie de Plombem. Experts en dépannage, réparation et installation de plomberie. Contactez-nous pour des solutions rapides et efficaces.",
+    url: "https://www.sebastien-morazzani.com",
+    siteName: "Plombem",
+    images: [
+      {
+        url: "https://plombem.vercel.app/imgOG.jpg",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
